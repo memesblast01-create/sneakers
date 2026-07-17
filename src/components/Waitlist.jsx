4946f14a8026f7aa58872c722d9f712d1508764a
memesblast01-create <memesblast01@gmail.com>
2026-07-18 01:51:58 +0400

    Update Waitlist.jsx

--- src/components/Waitlist.jsx
+++ src/components/Waitlist.jsx
@@ -39,7 +39,7 @@ export default function Waitlist() {
               aria-label="Email address"
             />
             <button type="submit" className="btn btn--solid">
-              Join waitlist
+              Get early access
             </button>
           </form>
         )}
